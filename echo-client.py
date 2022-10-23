f0775030a4b74a02e32b4cbe6456ae858e3cd466
S-Samiksha <samikshasankar01@gmail.com>
2022-10-23 21:17:29 +0800

    virtual environment and encryption of client data

--- echo-client.py
+++ echo-client.py
@@ -1,4 +1,7 @@
 # Derived from https://realpython.com/python-sockets/#echo-client-and-server
+# Derived from https://medium.com/asecuritysite-when-bob-met-alice/bob-and-alice-play-mental-poker-in-a-world-where-they-trust-no-one-6cc557596169 
+
+#Application usage for Alice
 
 import socket
 from Crypto.Util.number import *
@@ -6,14 +9,33 @@ from Crypto import Random
 import Crypto
 import gmpy2
 import sys
+BITS=128
 
-HOST = "10.27.251.159"  # The server's hostname or IP address
+HOST = "127.0.0.1"  # The server's hostname or IP address
 PORT = 53142  # The port used by the server
+
+p = Crypto.Util.number.getPrime(BITS, randfunc=Crypto.Random.get_random_bytes) #get a random p prime 
+q = Crypto.Util.number.getPrime(BITS, randfunc=Crypto.Random.get_random_bytes) #get a random q prime
+n = p*q #n value this has to be shared between alice and bob 
+PHI=(p-1)*(q-1) #phi value 
+
+# RSA Encrytion keys
+e_bob=65537 #seems like this is the public key
+e_alice=65539 #seems like this is the public key 
+d_alice=(gmpy2.invert(e_bob, PHI))
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     val = input("Enter your value: ")
+    if (len(val)>1):
+        msg = val
+
+    m = bytes_to_long(msg.encode('utf-8'))
+    c1=pow(m,e_alice, n) #M^(e_alice) mod n it is to be noted that this is in integer format 
+    print("Sending Encrypted Value: ", c1)
+    valsend = str(c1)
+
     s.connect((HOST, PORT))
-    s.sendall(val.encode())
+    s.sendall(valsend.encode())
     data = s.recv(1024)
 
     print(f"Received {data!r}")
--- echo-server.py
+++ echo-server.py
@@ -2,7 +2,7 @@
 
 import socket
 
-HOST = "10.27.251.159"  # Standard loopback interface address (localhost)
+HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
 PORT = 53142  # Port to listen on (non-privileged ports are > 1023)
 
 while (True):
